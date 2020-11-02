3549a6bb66302f45aa193daa77505699b6f478c1
cimbul <cimbul@users.noreply.github.com>
2020-11-02 19:14:43 +0000

    deploy: 2ec61ce976fcbe016e3fa9233ed9ee5fd8753266

--- implementors/core/alloc/global/trait.GlobalAlloc.js
+++ implementors/core/alloc/global/trait.GlobalAlloc.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
+implementors["tartan_kernel"] = [{"text":"impl&lt;'a&gt; GlobalAlloc for Allocator&lt;'a&gt;","synthetic":false,"types":[]}];
 implementors["tartan_uefi"] = [{"text":"impl GlobalAlloc for BootAllocator","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
