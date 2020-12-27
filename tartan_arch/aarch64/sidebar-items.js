6545b7560fb2d60303161f90f7fc9a37940f4dfd
cimbul <cimbul@users.noreply.github.com>
2020-12-27 04:02:56 +0000

    deploy: 5307a7ead4fbc22cd3740bdb216ac06e43edf3f7

---
+++ tartan_arch/aarch64/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["ExceptionLevel","`EL`: Defines the privilege level of executing code. Higher values have more privileges."],["StackPointerSelect","Controls which stack pointer register is used when executing code at EL1 or higher."]],"mod":[["float","Floating-point control and status."],["interrupt","Exception and interrupt handling."]]});
