e65c0c7a2a1abf4c69d891ff08e375dd2c25f999
cimbul <cimbul@users.noreply.github.com>
2022-09-17 16:39:46 +0000

    deploy: 4ef8f273542e651b24b8dc08c971ff66e5a9c079

--- implementors/log/trait.Log.js
+++ implementors/log/trait.Log.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"tartan_uefi":[["impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.11/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"tartan_uefi/io/struct.Logger.html\" title=\"struct tartan_uefi::io::Logger\">Logger</a>&lt;'_&gt;"]]
+"tartan_uefi":[["impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.17/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"tartan_uefi/io/struct.Logger.html\" title=\"struct tartan_uefi::io::Logger\">Logger</a>&lt;'_&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
